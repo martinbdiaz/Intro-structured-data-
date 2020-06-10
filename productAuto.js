132e346ac1cca0b42f3cb50e7cd119fe65ea9791
martinbdiaz <47432458+martinbdiaz@users.noreply.github.com>
2020-06-10 14:57:30 -0500

    Create productAuto.js

---
+++ productAuto.js
@@ -0,0 +1,13 @@
+(function (){   
+  setTimeout(function(){
+    var imagen=skuJson_0.skus[0].image.slice(0,skuJson_0.skus[0].image.indexOf("?"));
+    
+    var precio =skuJson_0.skus[0].bestPrice.toString().slice(0,-2)
+    var data = {"@context": "https://schema.org/","@type": "Product","name": skuJson_0.name,"image": imagen,"description":$('.productDescriptionShort').text(), "sku": skuJson_0.skus[0].sku, "mpn": skuJson_0.productId, "brand": {"@type": "Thing", "name": "Vélez"}, "offers": {"@type": "Offer", "itemCondition": "https://schema.org/UsedCondition", "availability" : "http://schema.org/InStock", "priceCurrency": "COP","price":precio, "url" : "https://www.velez.com.co/ofertas"}, "url" : window.location.href}  
+    var script = document.createElement('script');
+    script.type = "application/ld+json";
+    script.innerHTML = JSON.stringify(data);
+    document.getElementsByTagName('head')[0].appendChild(script);
+
+  },100)
+}) (document);
